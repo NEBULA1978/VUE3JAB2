93935dca96576981eea4c55d24ee9703ddb6fa1a
NEBULA1978 <rpascualey@gmail.com>
2023-08-05 20:07:26 +0200

    Plantilla parte1 contador

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-
+  <h2>Cena {{ contador + 1 }}</h2>
+  <button @:click="siguiente">Siguiente ({{ contador + 1 }}/  total)</button>
 </template>
 
 <script setup>
-
+import { ref, computed } from "vue"
+import { productos } from "./datos.js"
+const contador=ref(0)
+const total = productos.length;
+const siguiente = () => {
+  // Increment the value of contador
+  contador.value++
+  // Check if contador is greater than or equal to total
+  if (contador.value >= total) {
+    // If so, reset contador back to 0
+    contador.value = 0;
+  }
+}
 
 </script>
 
